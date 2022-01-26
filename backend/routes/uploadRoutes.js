6d9e0b647f95dbf84de26914bbd87e986533830f
Taro Serigano <taroserigano@gmail.com>
2022-01-26 14:48:34 -0600

    Update uploadRoutes.js

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -5,11 +5,13 @@ const router = express.Router()
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
+    //set the destination folder
     cb(null, 'uploads/')
   },
   filename(req, file, cb) {
     cb(
       null,
+      //SET fiel's fields name, plus date, plus extracted file's extension type 
       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     )
   },
@@ -17,7 +19,9 @@ const storage = multer.diskStorage({
 
 function checkFileType(file, cb) {
   const filetypes = /jpg|jpeg|png/
+  //check if macthing with the specified filetypes 
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  //check if matching with mmimetype
   const mimetype = filetypes.test(file.mimetype)
 
   if (extname && mimetype) {
