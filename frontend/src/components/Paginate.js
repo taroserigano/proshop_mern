6e7a09cba1eae56dd9124ec1f96169da8c52a3ec
Taro Serigano <taroserigano@gmail.com>
2022-01-27 17:41:47 -0600

    Update Paginate.js

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Pagination } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+///
+
+///
+
+//
+
 
 const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
   return (
